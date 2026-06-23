776ab5dc03e1942e5ae541ccdd3f8512cde0d35a
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-06-23 14:42:01 -0500

    Add files via upload

--- src/door/door.ino
+++ src/door/door.ino
@@ -7,8 +7,12 @@ int pos = 0;
 
 void setup()
 {
-  // pin number 9
+  // Pin number 9
   myservo.attach(9);
+  // Center the servo motor
+  myservo.write(90);
+  // Wait for things to be setup
+  delay(5000);
 }
 
 void loop()
@@ -16,12 +20,16 @@ void loop()
   for (pos = 0; pos <= 90; pos += 1)
   {
     myservo.write(pos);
-    delay(100);
+    delay(10);
   }
+
+  delay(1000);
   
   for (pos = 90; pos >= 0; pos -= 1)
   {
     myservo.write(pos);
-    delay(100);
+    delay(10);
   }
+
+  delay(1000);
 }
