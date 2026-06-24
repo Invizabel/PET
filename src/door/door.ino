7c1c38d9e355938bca47fc205d6d835a641f2d50
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-06-23 19:31:16 -0500

    Add files via upload

--- src/door/door.ino
+++ src/door/door.ino
@@ -1,37 +1,60 @@
 // Docs: https://docs.arduino.cc/tutorials/generic/basic-servo-control/
 #include <Servo.h>
 
-Servo myservo;
+Servo myServo;
 
+// Position of servo motor:
 int pos = 0;
+// Feeding countdown counter:
+unsigned int counter = 0;
+// Used for the seven segment display and time in between feeds:
+unsigned long startTime = millis();
+// Get time last measured:
+unsigned long endTime = 0;
+// Declare how many seconds in an hour:
+unsigned long secondsToHours = 3600;
 
 void setup()
 {
-  // Pin number 9
-  myservo.attach(9);
-  // Center the servo motor (Important)
-  myservo.write(90);
-  // Wait for things to be setup
+  // Pin number 9:
+  myServo.attach(9);
+  // Center the servo motor (Important):
+  myServo.write(0);
+  // Wait for things to be setup:
   delay(5000);
 }
 
 void loop()
 {
-  // Rotate the servo motor 80 degrees
-  for (pos = 0; pos <= 80; pos += 1)
+  // Feed pet after 12 hours twice a day as per https://www.animalhumanesociety.org/resource/feeding-your-dog-or-cat:
+  if (counter == 0)
   {
-    myservo.write(pos);
-    delay(10);
+    // Rotate the servo motor 80 degrees to start dispensing food:
+    for (pos = 0; pos <= 135; pos += 1)
+    {
+      myServo.write(pos);
+      delay(10);
+    }
+
+    // Wait for food to dispense:
+    delay(3000);
+
+    // Rotate the servo motor 80 degrees to stop dispensing food:
+    for (pos = 135; pos >= 0; pos -= 1)
+    {
+      myServo.write(pos);
+      delay(10);
+    }
+
+    // Reset counter
+    counter = 12 * secondsToHours;
   }
 
-  delay(1000);
-
-  // Rotate the servo motor 80 degrees
-  for (pos = 80; pos >= 0; pos -= 1)
+  // Timer logic:
+  startTime = millis();
+  if (startTime - endTime >= 1000)
   {
-    myservo.write(pos);
-    delay(10);
+     endTime = startTime;
+     counter -= 1;
   }
-
-  delay(1000);
 }
